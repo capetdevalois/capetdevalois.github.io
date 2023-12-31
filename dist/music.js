0f1910dc7dce4a0bf14fb1d23aba81c3211d416c
capetdevalois <2703998852@qq.com>
2024-01-01 02:32:28 +0800

    Site updated: 2024-01-01 02:32:24

--- dist/music.js
+++ dist/music.js
@@ -9,7 +9,7 @@ const ap = new APlayer({
         {
             name: "騎士王の誇り",
             artist: "川井憲次",
-            url: "https://music.163.com/#/song?id=448119",
+            url: "http://music.163.com/song/media/outer/url?id=448119.mp3",
             cover:
                 "http://p1.music.126.net/U0Tf2ESHBauv2F9t1PgEZA==/109951163339713541.jpg",
         }
